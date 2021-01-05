a5634928deb0190bcaa13ba35e579395cb2a16f2
radutofan <radutofan1@gmail.com>
2021-01-05 17:02:18 +0200

    added react-tsparticles

--- src/GymApp.React/src/admin/particles/ParticlesBackground.tsx
+++ src/GymApp.React/src/admin/particles/ParticlesBackground.tsx
@@ -1,32 +1,94 @@
-import Particles from 'react-particles-js';
-import '../../App.css';
+import Particles from "react-tsparticles";
 
 const ParticlesBackground = () => {
-
-    return (
-        <div id="particles-js">
-            <Particles
-                params={{
-                    particles: {
-                        color: {
-                            value: '#6599FF'
-                        },
-                        line_linked: {
-                            color: {
-                                value: '#097054'
-                            }
-                        },
-                        number: {
-                            value: 75
-                        },
-                        size: {
-                            value: 5
-                        }
-                    }
-                }}
-            />
-        </div>
-    )
+        return (
+          <Particles
+            id="tsparticles"
+            options={{
+              background: {
+                color: {
+                  value: "#344966",
+                },
+              },
+              backgroundMode:{
+                  enable:true,
+                  zIndex:-1
+              },
+              fpsLimit: 60,
+              interactivity: {
+                detectsOn: "window",
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "repulse",
+                    distance: 1000
+                  },
+                  resize: true,
+                },
+                modes: {
+                  bubble: {
+                    distance: 400,
+                    duration: 2,
+                    opacity: 0.8,
+                    size: 40,
+                  },
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: "#f0f4ef",
+                },
+                links: {
+                  color: "#bfcc94",
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+                },
+                collisions: {
+                  enable: true,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outMode: "bounce",
+                  random: false,
+                  speed: 6,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    value_area: 500,
+                  },
+                  value: 80,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "circle",
+                },
+                size: {
+                  random: true,
+                  value: 5,
+                },
+              },
+              detectRetina: true,
+            }}
+          />
+        );
 }
 
 
